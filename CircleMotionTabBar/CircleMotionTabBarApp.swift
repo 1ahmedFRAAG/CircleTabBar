b72a27f025fb9c741dbd15cf31396b6e9cd382cb
ahmed fraag <ahmedfraag255@gmail.com>
2023-05-10 18:42:32 +0300

    Initial Commit

---
+++ CircleMotionTabBar/CircleMotionTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircleMotionTabBarApp.swift
+//  CircleMotionTabBar
+//
+//  Created by ahmed fraag on 10/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct CircleMotionTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
